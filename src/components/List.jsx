5fe9a87f259fe7219a856644ef31b2af2d8907be
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-12-15 17:25:15 +0100

    List update with new styles + searchbar

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,31 +2,58 @@ import recipesData from "../assets/recipes.json";
 import React, { useState } from "react";
 import "../styles/List-module-style.css";
 function List() {
-  const [recipes, setRecipes] = useState(recipesData);
-
-  const handleDelete = (currentId) => {
-    setRecipes(recipes.filter((recipe) => recipe.id !== currentId));
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState(recipesData);
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    console.log(e.target.value);
+    setSearchItem(searchTerm);
+    const filteredItems = recipesData.filter((recipe) =>
+      recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredUsers(filteredItems);
   };
-
+  const handleDelete = (currentId) => {
+    setFilteredUsers(filteredUsers.filter((recipe) => recipe.id !== currentId));
+  };
   return (
-    <div className="listCtn">
-      {recipes.map((recipe) => (
-        <div key={recipe.id} className="container">
-          <div className="imageCtn">
-            <img src={recipe.image} alt="image" />
+    <div>
+      <div>
+        <input
+          type="text"
+          value={searchItem}
+          onChange={handleInputChange}
+          placeholder="Type to search"
+        />
+      </div>
+      <div className="listCtn">
+        {filteredUsers.map((recipe) => (
+          <div key={recipe.id} className="container">
+            <div className="imageCtn">
+              <img src={recipe.image} alt="image" />
+            </div>
+            <div className="textCtn">
+              <h3>{recipe.name}</h3>
+              <p>Calories: {recipe.calories}</p>
+              <p>{recipe.servings}</p>
+              <button
+                class="button"
+                type="button"
+                onClick={() => handleDelete(recipe.id)}
+              >
+                Delete
+              </button>
+              <dev />
+              {/* <Link to="/edit">*/}
+              <button class="button" type="button">
+                Edit
+              </button>
+              {/* </Link>*/}
+            </div>
           </div>
-          <div className="textCtn">
-            <h3>{recipe.name}</h3>
-            <p>{recipe.calories}</p>
-            <p>{recipe.servings}</p>
-            <button type="button" onClick={() => handleDelete(recipe.id)}>
-              Delete
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
-
 export default List;
